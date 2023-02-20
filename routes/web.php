2d413ff669076ee9e6dc5df0f48de73671253de3
Shofiatul Ayu Anikaningrum <shofiaa060@gmail.com>
2023-02-20 10:21:12 +0700

    ShofiatulAyu 24_praktikum_web_lanjut_dua

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,16 +21,30 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {  	
-    echo "Selamat Datang"; 
-}); 
+// Route::get('/', function () {  	
+//     echo "Selamat Datang"; 
+// }); 
 
-Route::get('/about', function () {  	
-    echo "NIM : 2141720048 <br> 
-        NAMA : SHOFIATUL AYU ANIKANINGRUM"; 
-});
+// Route::get('/about', function () {  	
+//     echo "NIM : 2141720048 <br> 
+//         NAMA : SHOFIATUL AYU ANIKANINGRUM"; 
+// });
 
-Route::get('/articles/{id}', function ($id) {  	
-    echo "Halaman Artikel dengan ID " .$id; 
-});
+// Route::get('/articles/{id}', function ($id) {  	
+//     echo "Halaman Artikel dengan ID " .$id; 
+// });
+
+// Route::get('/', [PageController::class, 'index']);
+
+// Route::get('/about',[PageController::class, 'about']);
+
+// Route::get('/articles/{id}',[PageController::class, 'show']);
+
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about',[AboutController::class, 'about']);
+
+Route::get('/articles/{id}',[ArticleController::class, 'show']);
+
 
